c8ba462517469cd07fd201200cd846f44fc69712
chuboy <billju666@gmail.com>
2020-04-08 08:02:52 +0800

    marked

--- fileHandler.js
+++ fileHandler.js
@@ -40,3 +40,33 @@ function xml_string_to_json(xmlstr) {
     }
     return xml2json(srcDOM)
 };
+function beautifiedJsonHtml(){
+    let json = JSON.stringify(this.rows,null,2)
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match=>{
+        var cls = 'number'
+        if (/^"/.test(match)) {
+            cls = /:$/.test(match)?'key':'string'
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean'
+        } else if (/null/.test(match)) {
+            cls = 'null'
+        }
+        return '<span class="' + cls + '">' + match + '</span>'
+    });
+    // .string {
+    //     color: orange;
+    // }
+    // .number {
+    //     color: lightgreen;
+    // }
+    // .boolean {
+    //     color: blue;
+    // }
+    // .null {
+    //     color: deeppink;
+    // }
+    // .key {
+    //     color: lightblue;
+    // }
+}
